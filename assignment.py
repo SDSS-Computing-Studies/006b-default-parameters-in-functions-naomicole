5648c6e412cc5aa295a43720f518ead633d29c7b
naomicole <71406713+naomicole@users.noreply.github.com>
2020-10-09 12:58:52 -0700

    .

--- assignment.py
+++ assignment.py
@@ -24,10 +24,12 @@ def factorPair(a,b):
     return answer
 
 
-def cosineLaw():
-    pass
+def cosineLaw(sidea,sideb,anglec, by="True"):
+    sidec=(sidea**2)+(sideb**2)-2*sidea*sideb*math.cos(anglec)
+    c=math.sqrt(sidec)
+    return c
 
-def tworadians():
+def toradians():
     pass
 
 def solution():
@@ -36,3 +38,5 @@ def solution():
 def quadratic():
     pass
 
+x= cosineLaw(20,20,20)
+print(x)
