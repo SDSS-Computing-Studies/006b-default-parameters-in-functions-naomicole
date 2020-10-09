c139458de096b272c39468076d411c3d51e55efa
naomicole <71406713+naomicole@users.noreply.github.com>
2020-10-09 12:30:42 -0700

    .

--- assignment.py
+++ assignment.py
@@ -2,18 +2,37 @@
 
 import math
 
-def tempConversion():
+def tempConversion(a, by="C"):
+    f=((a*(9/5))+32)
+    y=f
+    answer=y
+    if by=="F":
+        c=(a-32)*(5/9)
+        y=c
+        answer=y
 
+    answer=round(y,1)
+    return answer
 
-def factorPair():
+def factorPair(a,b):
+    c=a/b
 
+    fList=[int(b),int(c)]
+    fList.sort()
+    answer=fList
+
+    return answer
 
 
 def cosineLaw():
+    pass
 
-def convertAngle():
+def tworadians():
+    pass
 
 def solution():
+    pass
 
 def quadratic():
+    pass
 
--- task2.py
+++ task2.py
@@ -1,2 +1,5 @@
 #!python3
+import assignment
 
+x= assignment.factorPair(24,12)
+print(x)
